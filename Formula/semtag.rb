dcfc20b4a64b6cd9ca52b4759cd49343b7e7f053
1efty <18194777+1efty@users.noreply.github.com>
2020-11-10 21:51:27 -0600

    Upgrade semtag

--- Formula/semtag.rb
+++ Formula/semtag.rb
@@ -1,9 +1,9 @@
 class Semtag < Formula
   desc "Tag your repository according to Semantic Versioning."
   homepage "https://github.com/1efty/semtag"
-  version "0.0.5"
-  url "https://github.com/1efty/semtag/archive/v0.0.5.tar.gz"
-  sha256 "96eec81d7358c53546e30d9181c4e9b0c88695b5cb502e61e935e9850b4d72a8"
+  version "0.0.6"
+  url "https://github.com/1efty/semtag/archive/v0.0.6.tar.gz"
+  sha256 "95cee2c511fba421364ac0ca68816522cf6e8adc8e87b3090b8e460d93a1c92f"
   license "MIT"
   head "https://github.com/semtag/semtag.git"
 
@@ -15,8 +15,12 @@ class Semtag < Formula
   depends_on "go@1.15" => :build
 
   def install
-    ENV["CGO_ENABLED"] = "0"
     system "go", "build", *std_go_args, "-ldflags", "-s -w", "-mod=vendor"
+
+
+    (bash_completion/"semtag").write `#{bin}/semtag completion bash`
+    (zsh_completion/"_semtag").write `#{bin}/semtag completion zsh`
+    (fish_completion/"semtag.fish").write `#{bin}/semtag completion fish`
   end
 
   test do
